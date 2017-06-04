308e348b2954f9f0be492868930a8b27766be130
Маздайщик <a.v.konovalov87@mail.ru>
2017-06-04 14:55:48 +0300

    FIXED: ошибка в скрипте bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,12 @@ mkdir -p bin
 source ./c-plus-plus.conf.sh
 source ./bin/platform-specific.sh
 
-APPENDER=rasl-appender/_rasl-appender$(platform_suffix)
+APPENDER=rasl-appender/_rasl-appender$(platform_exe_suffix)
 $CPPLINEE$APPENDER rasl-appender/rasl-appender.cpp || exit 1
 
 make_dir() {
   DIR=$1
-  TARGET=bin/$2$(platform_suffix)
+  TARGET=bin/$2$(platform_exe_suffix)
 
   $CPPLINEE$TARGET -Isrlib -DDONT_PRINT_STATISTICS \
     $DIR/*.cpp $(platform_subdir_lookup srlib)/refalrts-platform-specific.cpp \
