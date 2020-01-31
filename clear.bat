f975fde0f53a504b401cb3188ec6f032d0bb1159
Маздайщик <a.v.konovalov87@mail.ru>
2020-02-01 01:05:10 +0300

    Скрипты очиски удаляют *.dll и *.so

--- clear.bat
+++ clear.bat
@@ -1,3 +1,5 @@
 @echo off
 erase bin\*.exe
+if exist bin\*.dll erase bin\*.dll
+if exist bin\*.so erase bin\*.so
 erase lib\*.*-prefix
--- clear.sh
+++ clear.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -f bin/*-core bin/lexgen bin/rsl-decompiler
+rm -f bin/*-core bin/lexgen bin/rsl-decompiler bin/*.dll bin/*.so
 rm -f lib/*.*-prefix
